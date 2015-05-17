09ecf59760bd6c56738fdf8b44315b5c1cb45c02
vincentzhengdk <vincent2001@gmail.com>
2015-05-17 20:35:28 +0200

    https://github.com/vincentzhengdk/EFtest.git

---
+++ WebApplicationMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationMVC5.Startup))]
+namespace WebApplicationMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
